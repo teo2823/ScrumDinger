96033cec5df4dfa8e88b1edb589c29bab6bc1a0f
teo2823 <mateo2823@hotmail.com>
2024-04-09 23:10:06 -0500

    Add landing

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 10, total: 15)
+        VStack {
+            ProgressView(value: 7, total: 10)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.subheadline)
+                    Label("300", systemImage: "timer")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.subheadline)
+                    Label("300", systemImage: "hourglass")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {print("hola puto")}){
+                    Image(systemName: "forward.fill")
+                        .foregroundStyle(.black)
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
     }
 }
 
